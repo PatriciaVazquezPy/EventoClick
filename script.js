8b89826765e00deda32e51aeb2b771d4aedf30c1
PatriciaVazquezPy <131322973+PatriciaVazquezPy@users.noreply.github.com>
2023-07-05 09:49:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Cambiar el Login por el Logout al dar click*/ 
+function login(login){
+    login.innerText="Logout";
+}
+/*Remover el botón Add Definition */
+function add(add){
+    add.remove();
+}
+/*Alerta al dar click*/
+function likes(){
+    alert("Ninja was liked");
+}
